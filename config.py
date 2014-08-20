3345ddbae197d19abec3de81e51b6e137bf51327
Kevin Glidewell <kpglide@gmail.com>
2014-08-19 22:16:55 -0400

    updated config

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_SENDER = 'TSports Legal Portal Admin <kpglide@gmail.com>'
