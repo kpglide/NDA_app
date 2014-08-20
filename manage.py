047cd5f35834bd346fcb66c76fc0c1649dc53112
Kevin Glidewell <kpglide@gmail.com>
2014-08-19 21:09:25 -0400

    added upgrade command to manager

--- manage.py
+++ manage.py
@@ -17,5 +17,13 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+	def deploy():
+	"""Run deployment tasks."""
+	from flask.ext.migrate import upgrade
+	from app.models import Role, User
+	# migrate database to latest revision
+	upgrade()
+
 if __name__ == '__main__':
 	manager.run()
